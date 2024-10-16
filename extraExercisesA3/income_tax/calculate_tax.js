4669fe31baa16faa99007131f8ef5dc440669221
THarris06 <144939694+THarris06@users.noreply.github.com>
2024-10-16 09:41:14 -0230

    commit

--- extraExercisesA3/income_tax/calculate_tax.js
+++ extraExercisesA3/income_tax/calculate_tax.js
@@ -3,17 +3,17 @@
 const $ = selector => document.querySelector(selector);
 
 const processEntry = () => {
-	const userEntry = parseFloat($("#income").value);
+	const userEntry = parseInt($("#income").value);
     if (userEntry < 0 || isNaN(userEntry)) {
         alert("Income must be a valid number greater than 0")
     }
     else {
-		const taxes = calculateTax(userEntry)
-		$("tax").value = taxes 
+		$("#tax").value = calculateTax(userEntry).toFixed(2)
     }
-}
+	$("#income").focus();
+};
 
-function calculateTax(income) {
+const calculateTax = (income) => {
 	let tax = 0
 	if (income <= 9875) {
 		tax = income * 0.10
@@ -21,6 +21,26 @@ function calculateTax(income) {
 	else if (income <= 40125) {
 		tax = 987.5
 		tax += (income - 9875) * 0.12
+	}
+	else if (income <= 85525) {
+		tax = 4617.5
+		tax += (income - 40125) * 0.22
+	}
+	else if (income <= 163300) {
+		tax = 14605.5
+		tax += (income - 85525) * 0.24
+	}
+	else if (income <= 207350) {
+		tax = 33271.5
+		tax += (income - 163300) * 0.32
+	}
+	else if (income <= 518400) {
+		tax = 47367.5
+		tax += (income - 207350) * 0.35
+	}
+	else {
+		tax = 156235
+		tax += (income - 518400) * 0.37
 	}
 	return tax;
 };
