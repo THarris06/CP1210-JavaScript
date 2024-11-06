b50b62fc72c0e54e92290845ad95ca08db5ba91c
THarris06 <144939694+THarris06@users.noreply.github.com>
2024-11-06 08:35:04 -0330

    commit

--- ReviewExOct31/exercise.js
+++ ReviewExOct31/exercise.js
@@ -1,3 +1,27 @@
+"use strict";
+const $ = selector => document.querySelector(selector);
+
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("loaded");
-})
+    let timer = null;
+    let counter = 1;
+
+    $("#toggleShow").addEventListener("click", () => {
+        if (timer == null) {
+            timer = setInterval( () => {
+                counter++;
+                $("#number").textContent = counter;
+            }, 1000)
+            $("#toggleShow").value = "Stop";
+        }
+        else {
+            clearInterval(timer);
+            timer = null;
+            $("#toggleShow").value = "Start";
+        }
+    });
+
+    $("#reset").addEventListener("click", () => {
+        counter = 1;
+        $("#number").textContent = counter;
+    });
+});
